b2ba7a69064b85892e8c065970544decd843452c
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2024-09-14 16:25:36 -0300

    Configurando Eureka Server

--- src/main/java/br/com/fiap/server/ServerApplication.java
+++ src/main/java/br/com/fiap/server/ServerApplication.java
@@ -2,8 +2,10 @@ package br.com.fiap.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer //define que é uma aplicacao Servidor do Eureka
 public class ServerApplication {
 
 	public static void main(String[] args) {
